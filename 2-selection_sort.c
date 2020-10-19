86cf8283ba5299c137ad857408867e65b3fd67bd
Sofiag8 <1943@holbertonschool.com>
2020-10-19 23:44:04 +0000

    condition arrays less than 2 and min number differente from the traverser

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,7 +10,7 @@ void selection_sort(int *array, size_t size)
 	size_t traverser, picker, min_idx;
 	int tmp;
 
-	if (!array)
+	if (!array || size < 2)
 		return;
 	for (traverser = 0; traverser < size - 1; traverser++)
 	{
@@ -18,10 +18,12 @@ void selection_sort(int *array, size_t size)
 		for (picker = traverser + 1; picker < size; picker++)
 			if (array[picker] < array[min_idx])
 				min_idx = picker;
-		tmp = array[min_idx];
-		array[min_idx] = array[traverser];
-		array[traverser] = tmp;
-
-		print_array(array, size);
+		if (traverser != min_idx)
+		{
+			tmp = array[min_idx];
+			array[min_idx] = array[traverser];
+			array[traverser] = tmp;
+			print_array(array, size);
+		}
 	}
 }
