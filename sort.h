ee03b9d360c7894dc4709e9e57f841e4d861fc39
Sofiag8 <1943@holbertonschool.com>
2020-10-20 00:58:20 +0000

    quick sort

--- sort.h
+++ sort.h
@@ -24,5 +24,9 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+int partition(int *array, int less_p, int greater_p, size_t size);
+void quicksort(int *array, int less_p, int greater_p, size_t size);
 
 #endif
